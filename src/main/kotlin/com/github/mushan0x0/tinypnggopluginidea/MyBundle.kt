358f762d91c3987affa6bb1f768f4a491bb791e2
GitHub Action <action@github.com>
2022-08-14 07:50:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/mushan0x0/tinypnggopluginidea/MyBundle.kt
+++ src/main/kotlin/com/github/mushan0x0/tinypnggopluginidea/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mushan0x0.tinypnggopluginidea
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/mushan0x0/tinypnggopluginidea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mushan0x0/tinypnggopluginidea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mushan0x0.tinypnggopluginidea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mushan0x0.tinypnggopluginidea.MyBundle
 
 class MyApplicationService {
 
